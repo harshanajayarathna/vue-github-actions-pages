2b38a6afa3563afab945445b25d3067012efabcc
harshanajayarathna <harshanajayarathna@gmail.com>
2021-03-10 13:07:28 +1300

    Fourth deployment to AWS Amplify

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,18 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <h2>Hooray, I'm on AWS and deployed via Github + Amplify with built-in CI/CD pipeline</h2>
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
 export default {
   name: 'Home',
   components: {
+    HelloWorld
   }
 }
 </script>
